b13cd43827f3adc64caf825554bb9c6322719997
esesil <hesies119@gmail.com>
2021-01-01 02:40:35 +0900

    은실/ 오류 수정

---
+++ src/main/java/com/kh/ontact/commute/model/dto/CommuteDto.java
@@ -0,0 +1,15 @@
+package com.kh.ontact.commute.model.dto;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.kh.ontact.chatcontent.model.dto.ChatContentDto;
+
+import lombok.Data;
+
+@Data
+@Component
+public class CommuteDto {
+
+}
